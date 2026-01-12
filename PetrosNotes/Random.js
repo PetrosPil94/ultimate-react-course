ad37dbd536166c3e33aa7074b7339c2a9fc33517
PetrosPilitsidis <petrospilitsidis1994@gmail.com>
2026-01-12 22:34:13 +0200

    add notes

---
+++ PetrosNotes/Random.js
@@ -0,0 +1,12 @@
+// - Reasons why Javascript front-end frameworks exist
+// 1) They enforce a correct way of structuring and writing code (to tackle the spaghetti code effect)
+// 2) They give a consistent way of building web applications
+// 3) It hard with vanilla js to keep the data in-sync with the UI
+
+// - What is React?
+
+// Definition = Extremely popular declarative component based, state driven, javascript library for building interfaces created by facebook.
+// JSX -> A declarative syntax (Combination of js, css, html)
+
+// How react updates UI?
+// -> State driven (Re-render components)
