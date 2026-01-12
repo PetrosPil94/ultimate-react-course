032dc6cbe09ef11d974a97bb2df144de19808c2c
PetrosPilitsidis <petrospilitsidis1994@gmail.com>
2026-01-12 23:21:13 +0200

    add notes

---
+++ PetrosNotes/Setting a react app.js
@@ -0,0 +1,14 @@
+// -> Setting up a new react project
+
+// 1) Create-react-app
+
+// Complete starter kit for react applications -> Everything is already configured
+// Cons -> It was developed years ago so it is kinda outdated. (Not for real-world app ONLY for learning)
+
+// 2) VITE
+
+// Perfect for real-world apps
+
+// Modern build tool that contains a template for setting up react applications
+// Manually set up ESLint (and others).
+// Extremely fast hot module replacement (Fast reload on code changes)
