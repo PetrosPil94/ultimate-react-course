01cf091d9cb60e5e768b9a33e19166c2b08fe748
PetrosPilitsidis <petrospilitsidis1994@gmail.com>
2026-01-20 22:11:14 +0200

    additions

---
+++ PetrosNotes/components.js
@@ -0,0 +1,9 @@
+// React applications are entirely made out of components.
+// Building blocks of user interfaces in Reacts.
+// Piece of ui that has its own data, logic and appearance.
+// We build complex UIs by building multiple components and combining them.
+// Components can be reused, nested inside each other and pass data between them (props).
+
+// Component tree (Term to analyze complex UIs by noticing parent components and child components)
+
+// Creating and Reusing a component
